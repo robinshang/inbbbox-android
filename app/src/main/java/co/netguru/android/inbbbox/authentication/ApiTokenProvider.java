44936c93bffef0e60525265e70e9505288123b40
mmarkiew <maciej.markiew@gmail.com>
2016-10-12 12:53:36 +0200

    typo

--- app/src/main/java/co/netguru/android/inbbbox/authentication/ApiTokenProvider.java
+++ app/src/main/java/co/netguru/android/inbbbox/authentication/ApiTokenProvider.java
@@ -14,7 +14,6 @@ import co.netguru.android.inbbbox.data.api.AuthorizeApi;
 import co.netguru.android.inbbbox.data.models.TokenResponse;
 import co.netguru.android.inbbbox.utils.Constants;
 import rx.Observable;
-import rx.functions.Action1;
 
 public class ApiTokenProvider {
 
@@ -28,10 +27,10 @@ public class ApiTokenProvider {
 
     public Observable<TokenResponse> getToken(String code) {
         return api.getToken(Constants.OAUTH.CLIENT_ID_KEY, Constants.OAUTH.CLIENT_SECRET_KEY, code)
-                .doOnNext(tokenResponse -> saveTokenToDn(tokenResponse));
+                .doOnNext(tokenResponse -> saveTokenToDB(tokenResponse));
     }
 
-    private void saveTokenToDn(TokenResponse tokenResponse) {
+    private void saveTokenToDB(TokenResponse tokenResponse) {
         // TODO: 11.10.2016 caching token
     }
 }
