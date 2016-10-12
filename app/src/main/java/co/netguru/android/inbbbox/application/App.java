79af668aa8b6ad1836e44690dbeace54983f0720
mmarkiew <maciej.markiew@gmail.com>
2016-10-12 09:14:27 +0200

    template reconfig

---
+++ app/src/main/java/co/netguru/android/inbbbox/application/App.java
@@ -0,0 +1,35 @@
+
+package co.netguru.android.inbbbox.application;
+
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * Base application class
+ * Created by lukaszjanyga on 08/09/16.
+ */
+public class App extends Application {
+
+    private ApplicationComponent appComponent;
+
+    public static ApplicationComponent getAppComponent(Context context) {
+        return ((App) context.getApplicationContext()).appComponent;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        appComponent.getDebugMetricsHelper().init(this);
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+
+        // init dagger appComponent
+        this.appComponent = DaggerApplicationComponent
+                .builder()
+                .build();
+    }
+
+}
---
+++ app/src/release/java/co/netguru/android/inbbbox/application/DebugMetricsHelper.java
@@ -0,0 +1,31 @@
+package co.netguru.android.inbbbox.application;
+
+import android.content.Context;
+
+import javax.inject.Inject;
+
+/**
+ * Helper class that initializes a set of debugging tools
+ * for the debug build type and does nothing for the release type.
+ * <p>
+ * Tools:
+ * <ul>
+ * <li> AndroidDevMetrics
+ * <li> Stetho
+ * <li> StrictMode
+ * <li> LeakCanary
+ * </ul>
+ * <p>
+ * Created by lukaszjanyga on 08/09/16.
+ */
+public class DebugMetricsHelper {
+
+    @Inject
+    public DebugMetricsHelper() {
+    }
+
+    public void init(Context context) {
+        //dummy - no debug tools for release builds
+    }
+
+}
