52784d7fa163c6399c4f7d085737733f539c574f
mmarkiew <maciej.markiew@gmail.com>
2016-10-13 22:24:00 +0200

    missing file for mock flavour

---
+++ app/src/mock/java/co/netguru/android/inbbbox/data/api/AuthorizeApiModule.java
@@ -0,0 +1,38 @@
+/*
+ * Created by Maciej Markiewicz
+ * Copyright (c) 2016.
+ * netguru.co
+ *
+ *
+ */
+
+package co.netguru.android.inbbbox.data.api;
+
+import com.google.gson.Gson;
+
+import javax.inject.Singleton;
+
+import co.netguru.android.inbbbox.utils.Constants;
+import dagger.Module;
+import dagger.Provides;
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+@Module
+public class AuthorizeApiModule {
+
+    @Singleton
+    @Provides
+    AuthorizeApi provideAuthorizeApi(OkHttpClient okHttpClient, Gson gson) {
+        // TODO: 13.10.2016 implement mock for tests
+        return new Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .baseUrl(Constants.OAUTH.OAUTH_BASE_URL)
+                .client(okHttpClient)
+                .build()
+                .create(AuthorizeApi.class);
+    }
+}
