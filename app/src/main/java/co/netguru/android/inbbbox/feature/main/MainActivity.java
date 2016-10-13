cdf53514e1c474d972ddff481e757013d5b85b0c
Rafal Adasiewicz <rafal.adasiewicz@netguru.pl>
2016-10-13 15:15:32 +0200

    [IA-17] Add toolbar to main activity

--- app/src/main/java/co/netguru/android/inbbbox/feature/main/MainActivity.java
+++ app/src/main/java/co/netguru/android/inbbbox/feature/main/MainActivity.java
@@ -2,6 +2,9 @@ package co.netguru.android.inbbbox.feature.main;
 
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 import butterknife.BindView;
 import co.netguru.android.inbbbox.R;
@@ -11,6 +14,8 @@ import co.netguru.android.inbbbox.view.NonSwipeableViewPager;
 
 public class MainActivity extends BaseActivity {
 
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
     @BindView(R.id.main_tab_layout)
     TabLayout tabLayout;
     @BindView(R.id.main_view_pager)
@@ -23,6 +28,24 @@ public class MainActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initializePager();
+        initializeToolbar();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(android.view.MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // TODO: 13.10.2016  Add action on menu item click
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     private void initializePager() {
@@ -32,6 +55,14 @@ public class MainActivity extends BaseActivity {
         tabLayout.setupWithViewPager(viewPager);
         for (final MenuItem item : MenuItem.values()) {
             tabLayout.getTabAt(item.getPosition()).setIcon(item.getIcon());
+        }
+    }
+
+    private void initializeToolbar() {
+        setSupportActionBar(toolbar);
+        final ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayShowTitleEnabled(false);
         }
     }
 }
