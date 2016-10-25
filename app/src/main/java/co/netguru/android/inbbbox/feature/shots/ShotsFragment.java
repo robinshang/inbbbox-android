73710e05bba36b858e5f0f3b24567c0635d1bf43
mmarkiew <maciej.markiew@gmail.com>
2016-10-25 13:03:48 +0200

    [IA-3] butterknife and recycler

--- app/src/main/java/co/netguru/android/inbbbox/feature/shots/ShotsFragment.java
+++ app/src/main/java/co/netguru/android/inbbbox/feature/shots/ShotsFragment.java
@@ -3,13 +3,19 @@ package co.netguru.android.inbbbox.feature.shots;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import co.netguru.android.inbbbox.R;
 
 public class ShotsFragment extends Fragment {
+
+    @BindView(R.id.shots_recycler_view)
+    RecyclerView shotsRecyclerView;
 
     public static ShotsFragment newInstance() {
         return new ShotsFragment();
@@ -20,5 +26,11 @@ public class ShotsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_shots, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ButterKnife.bind(this, view);
     }
 }
