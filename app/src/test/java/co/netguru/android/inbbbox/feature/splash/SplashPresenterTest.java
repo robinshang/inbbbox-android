07dca1f0e82620da2e360252399f4179292f72e4
mmarkiew <maciej.markiew@gmail.com>
2016-10-25 09:04:56 +0200

    [IA-136] test suit added

---
+++ app/src/test/java/co/netguru/android/inbbbox/feature/splash/SplashPresenterTest.java
@@ -0,0 +1,14 @@
+package co.netguru.android.inbbbox.feature.splash;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SplashPresenterTest {
+
+    @Test
+    void when_then() {
+
+    }
+}
