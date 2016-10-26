6bcbc7bc7c7eac7fc056164008cf46533ea3aa55
mmarkiew <maciej.markiew@gmail.com>
2016-10-26 16:16:09 +0200

    SwipeanleImagePanel

--- app/src/main/java/co/netguru/android/inbbbox/feature/shots/recycler/ShotsViewHolder.java
+++ app/src/main/java/co/netguru/android/inbbbox/feature/shots/recycler/ShotsViewHolder.java
@@ -2,27 +2,18 @@ package co.netguru.android.inbbbox.feature.shots.recycler;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.ImageView;
-
-import com.daimajia.swipe.SwipeLayout;
-import com.daimajia.swipe.SwipeLayout.SwipeListener;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import co.netguru.android.inbbbox.R;
 import co.netguru.android.inbbbox.data.ui.Shot;
 import co.netguru.android.inbbbox.utils.imageloader.ImageLoader;
+import co.netguru.android.inbbbox.view.SwipeableImagePanel;
 
 public class ShotsViewHolder extends RecyclerView.ViewHolder {
 
-    @BindView(R.id.iv_shot_image)
-    ImageView shotImageView;
-
-    @BindView(R.id.swipe_layout)
-    SwipeLayout swipeLayout;
-
-    @BindView(R.id.bottom_wrapper)
-    View bottomWrapper;
+    @BindView(R.id.swipeable_image_panel)
+    SwipeableImagePanel imagePanel;
 
     private ImageLoader imageLoader;
 
@@ -30,46 +21,6 @@ public class ShotsViewHolder extends RecyclerView.ViewHolder {
         super(itemView);
         this.imageLoader = imageLoader;
         ButterKnife.bind(this, itemView);
-        initSwipeLayout();
-    }
-
-    private void initSwipeLayout() {
-        swipeLayout.setShowMode(SwipeLayout.ShowMode.LayDown);
-
-//add drag edge.(If the BottomView has 'layout_gravity' attribute, this line is unnecessary)
-        swipeLayout.addDrag(SwipeLayout.DragEdge.Right, bottomWrapper);
-        swipeLayout.addSwipeListener(new SwipeListener() {
-            @Override
-            public void onClose(SwipeLayout layout) {
-                //when the SurfaceView totally cover the BottomView.
-            }
-
-            @Override
-            public void onUpdate(SwipeLayout layout, int leftOffset, int topOffset) {
-                //you are swiping.
-            }
-
-            @Override
-            public void onStartOpen(SwipeLayout layout) {
-
-            }
-
-            @Override
-            public void onOpen(SwipeLayout layout) {
-                //when the BottomView totally show.
-            }
-
-            @Override
-            public void onStartClose(SwipeLayout layout) {
-
-            }
-
-            @Override
-            public void onHandRelease(SwipeLayout layout, float xvel, float yvel) {
-                //when user's hand released.
-                swipeLayout.close(true);
-            }
-        });
     }
 
     void bind(Shot shot) {
@@ -79,6 +30,6 @@ public class ShotsViewHolder extends RecyclerView.ViewHolder {
     private void setupImage(Shot shot) {
         float radius = itemView.getContext().getResources().getDimension(R.dimen.shot_corner_radius);
         imageLoader.enableRoundCornersTransformationForNextRequest(radius);
-        imageLoader.loadImageWithThumbnail(shotImageView, shot);
+        imageLoader.loadImageWithThumbnail(imagePanel.getImageView(), shot);
     }
 }
---
+++ app/src/main/java/co/netguru/android/inbbbox/view/SwipeableImagePanel.java
@@ -0,0 +1,101 @@
+package co.netguru.android.inbbbox.view;
+
+import android.content.Context;
+import android.os.Handler;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.daimajia.swipe.SwipeLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import co.netguru.android.inbbbox.R;
+
+public class SwipeableImagePanel extends LinearLayout {
+
+    private static final long AUTO_CLOSE_DELAY = 600;
+
+    @BindView(R.id.iv_shot_image)
+    ImageView shotImageView;
+
+    @BindView(R.id.swipe_layout)
+    SwipeLayout swipeLayout;
+
+    @BindView(R.id.bottom_wrapper)
+    View bottomWrapper;
+
+    private View rootView;
+    private Handler closeHandler = new Handler();
+
+    public SwipeableImagePanel(Context context) {
+        super(context);
+        initSwipableView();
+    }
+
+    public SwipeableImagePanel(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initSwipableView();
+    }
+
+    public SwipeableImagePanel(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initSwipableView();
+    }
+
+    private void initSwipableView() {
+        rootView = LayoutInflater.from(getContext()).inflate(R.layout.swipeable_image_panel_layout, this, true);
+        ButterKnife.bind(rootView, this);
+
+        setupSwipeView();
+    }
+
+    private void setupSwipeView() {
+        swipeLayout.setShowMode(SwipeLayout.ShowMode.LayDown);
+
+//add drag edge.(If the BottomView has 'layout_gravity' attribute, this line is unnecessary)
+        swipeLayout.addDrag(SwipeLayout.DragEdge.Right, bottomWrapper);
+        swipeLayout.addSwipeListener(new SwipeLayout.SwipeListener() {
+            @Override
+            public void onClose(SwipeLayout layout) {
+                //when the SurfaceView totally cover the BottomView.
+            }
+
+            @Override
+            public void onUpdate(SwipeLayout layout, int leftOffset, int topOffset) {
+                //you are swiping.
+            }
+
+            @Override
+            public void onStartOpen(SwipeLayout layout) {
+
+            }
+
+            @Override
+            public void onOpen(SwipeLayout layout) {
+                //when the BottomView totally show.
+            }
+
+            @Override
+            public void onStartClose(SwipeLayout layout) {
+
+            }
+
+            @Override
+            public void onHandRelease(SwipeLayout layout, float xvel, float yvel) {
+                //when user's hand released.
+                delayClose();
+            }
+        });
+    }
+
+    private void delayClose() {
+        closeHandler.postDelayed(() -> swipeLayout.close(true, true), AUTO_CLOSE_DELAY);
+    }
+
+    public ImageView getImageView() {
+        return shotImageView;
+    }
+}
