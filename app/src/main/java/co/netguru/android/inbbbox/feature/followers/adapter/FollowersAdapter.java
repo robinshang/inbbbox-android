9bbbe42e59598efe40d4df4b17d224315abaacbe
Rafal Adasiewicz <rafal.adasiewicz@netguru.pl>
2016-11-04 11:38:20 +0100

    [IA-21] Add followers adapter

---
+++ app/src/main/java/co/netguru/android/inbbbox/feature/followers/adapter/FollowersAdapter.java
@@ -0,0 +1,57 @@
+package co.netguru.android.inbbbox.feature.followers.adapter;
+
+import android.support.annotation.LayoutRes;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import co.netguru.android.inbbbox.R;
+
+public class FollowersAdapter extends RecyclerView.Adapter<FollowersViewHolder> {
+
+    private List<Object> followersList;
+
+    @Inject
+    public FollowersAdapter() {
+        followersList = new ArrayList<>();
+    }
+
+    @Override
+    public FollowersViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        final View view = LayoutInflater.from(parent.getContext()).inflate(getViewHolderLayout(parent), parent, false);
+        return new FollowersViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(FollowersViewHolder holder, int position) {
+        holder.bind(followersList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return followersList.size();
+    }
+
+    public void setFollowersList(List<Object> followersList) {
+        this.followersList.clear();
+        this.followersList.addAll(followersList);
+        notifyDataSetChanged();
+    }
+
+    @LayoutRes
+    private int getViewHolderLayout(ViewGroup viewGroup) {
+        RecyclerView.LayoutManager layoutManager = ((RecyclerView) viewGroup).getLayoutManager();
+        if (layoutManager == null) {
+            throw new IllegalStateException("Recycler view should have layout manager already!");
+        }
+        return layoutManager instanceof GridLayoutManager
+                ? R.layout.follower_item_grid_view : R.layout.follower_item_list_view;
+    }
+}
---
+++ app/src/main/java/co/netguru/android/inbbbox/feature/followers/adapter/FollowersViewHolder.java
@@ -0,0 +1,17 @@
+package co.netguru.android.inbbbox.feature.followers.adapter;
+
+import android.view.View;
+
+import co.netguru.android.inbbbox.feature.common.BaseViewHolder;
+
+public class FollowersViewHolder extends BaseViewHolder<Object> {
+
+    public FollowersViewHolder(View view) {
+        super(view);
+    }
+
+    @Override
+    public void bind(Object item) {
+
+    }
+}
