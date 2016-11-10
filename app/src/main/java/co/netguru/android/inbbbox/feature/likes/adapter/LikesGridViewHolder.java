e9c1ea6b7db8c9476296c2e957d733069bd217fd
mmarkiew <maciej.markiew@gmail.com>
2016-11-10 15:23:51 +0100

    [IA-140] review changes

--- app/src/main/java/co/netguru/android/inbbbox/feature/likes/adapter/LikesGridViewHolder.java
+++ app/src/main/java/co/netguru/android/inbbbox/feature/likes/adapter/LikesGridViewHolder.java
@@ -4,17 +4,17 @@ import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
 import com.bumptech.glide.Glide;
-import com.makeramen.roundedimageview.RoundedImageView;
 
 import butterknife.BindView;
 import co.netguru.android.inbbbox.R;
 import co.netguru.android.inbbbox.feature.common.BaseViewHolder;
 import co.netguru.android.inbbbox.model.ui.LikedShot;
+import co.netguru.android.inbbbox.view.RoundedCornersImageView;
 
 public class LikesGridViewHolder extends BaseViewHolder<LikedShot> {
 
     @BindView(R.id.like_item_image_view)
-    RoundedImageView imageView;
+    RoundedCornersImageView imageView;
 
     LikesGridViewHolder(ViewGroup parent) {
         super(LayoutInflater.from(parent.getContext()).inflate(R.layout.like_item_list_view, parent, false));
@@ -22,6 +22,8 @@ public class LikesGridViewHolder extends BaseViewHolder<LikedShot> {
 
     @Override
     public void bind(LikedShot item) {
+        final float radius = itemView.getResources().getDimension(R.dimen.shot_corner_radius);
+        imageView.setRadius(radius);
         Glide.with(itemView.getContext())
                 .load(item.getImageUrl())
                 .placeholder(R.drawable.shot_placeholder)
