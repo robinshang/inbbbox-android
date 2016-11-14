8706a745c1fc92b29f5f7f32e81df23a93c96f8a
Rafal Adasiewicz <rafal.adasiewicz@netguru.pl>
2016-11-14 13:29:11 +0100

    [IA-32] Add toolbar to follower details activity

--- app/src/main/java/co/netguru/android/inbbbox/feature/followers/details/FollowerDetailsActivity.java
+++ app/src/main/java/co/netguru/android/inbbbox/feature/followers/details/FollowerDetailsActivity.java
@@ -4,7 +4,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import butterknife.BindView;
 import co.netguru.android.inbbbox.R;
 import co.netguru.android.inbbbox.feature.common.BaseActivity;
 import co.netguru.android.inbbbox.model.ui.Follower;
@@ -13,6 +18,9 @@ import co.netguru.android.inbbbox.model.ui.Follower;
 public class FollowerDetailsActivity extends BaseActivity {
 
     private static final String FOLLOWER_KEY = "follower_key";
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
     public static void startActivity(Context context, Follower follower) {
         final Intent intent = new Intent(context, FollowerDetailsActivity.class);
@@ -24,10 +32,36 @@ public class FollowerDetailsActivity extends BaseActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_follower_details);
+        initializeToolbar();
         if (savedInstanceState == null && getIntent().hasExtra(FOLLOWER_KEY)) {
             replaceFragment(R.id.follower_details_fragment_container,
                     FollowerDetailsFragment.newInstance(getIntent().getParcelableExtra(FOLLOWER_KEY)), FollowerDetailsFragment.TAG).commit();
         }
     }
-    // TODO: 14.11.2016 Init actionbar
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.follower_details_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_follow:
+                // TODO: 14.11.2016 Add action on action follow clicked
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void initializeToolbar() {
+        toolbar.setTitleTextColor(getResources().getColor(R.color.white));
+        setSupportActionBar(toolbar);
+        final ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
 }
