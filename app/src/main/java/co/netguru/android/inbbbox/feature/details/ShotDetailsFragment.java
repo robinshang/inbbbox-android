4dc0c73448a972c1d26b06d39fc03412d81e3079
mmarkiew <maciej.markiew@gmail.com>
2016-11-15 16:11:53 +0100

    [IA-18] mocked data class

--- app/src/main/java/co/netguru/android/inbbbox/feature/details/ShotDetailsFragment.java
+++ app/src/main/java/co/netguru/android/inbbbox/feature/details/ShotDetailsFragment.java
@@ -14,11 +14,6 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 
-import org.threeten.bp.LocalDateTime;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.inject.Inject;
 
 import butterknife.BindView;
@@ -30,7 +25,6 @@ import co.netguru.android.inbbbox.di.component.ShotDetailsComponent;
 import co.netguru.android.inbbbox.di.module.ShotsDetailsModule;
 import co.netguru.android.inbbbox.feature.details.recycler.DetailsViewActionCallback;
 import co.netguru.android.inbbbox.feature.details.recycler.ShotDetailsAdapter;
-import co.netguru.android.inbbbox.model.ui.Comment;
 import co.netguru.android.inbbbox.model.ui.ShotDetails;
 import co.netguru.android.inbbbox.utils.ThumbnailUtil;
 import co.netguru.android.inbbbox.view.RoundedCornersImageView;
@@ -60,25 +54,25 @@ public class ShotDetailsFragment extends BottomSheetDialogFragment {
         @Override
         public void onCompanySelected(String companyProfileUrl) {
             // TODO: 15.11.2016
-            Toast.makeText(getContext(), "company", Toast.LENGTH_SHORT).show();
+            Toast.makeText(getContext(), "company: " + companyProfileUrl, Toast.LENGTH_SHORT).show();
         }
 
         @Override
         public void onUserSelected(int userId) {
             // TODO: 15.11.2016
-            Toast.makeText(getContext(), "user", Toast.LENGTH_SHORT).show();
+            Toast.makeText(getContext(), "userId " + userId, Toast.LENGTH_SHORT).show();
         }
 
         @Override
         public void onShotLikeAction(int shotId, boolean isLiked) {
             // TODO: 15.11.2016
-            Toast.makeText(getContext(), "like", Toast.LENGTH_SHORT).show();
+            Toast.makeText(getContext(), "like: " + isLiked, Toast.LENGTH_SHORT).show();
         }
 
         @Override
         public void onShotBucket(int shotId, boolean isLikedBucket) {
             // TODO: 15.11.2016
-            Toast.makeText(getContext(), "bucket", Toast.LENGTH_SHORT).show();
+            Toast.makeText(getContext(), "bucket: " + isLikedBucket, Toast.LENGTH_SHORT).show();
         }
     };
 
@@ -106,117 +100,10 @@ public class ShotDetailsFragment extends BottomSheetDialogFragment {
         ButterKnife.bind(this, view);
         initRecycler();
 
-        // TODO: 14.11.2016 FOR TESTS
-        List<Comment> comments = new ArrayList<>();
-        Comment sec = Comment.builder()
-                .author("sec")
-                .authorAvatarUrl("http://findicons.com/files/icons/1072/face_avatars/300/i03.png")
-                .date(LocalDateTime.now())
-                .text("Example comments")
-                .build();
-
-        Comment fewSec = Comment.builder()
-                .author("fewSec")
-                .authorAvatarUrl("http://findicons.com/files/icons/1072/face_avatars/300/i03.png")
-                .date(LocalDateTime.now().plusSeconds(20))
-                .text("Example comments")
-                .build();
-
-        Comment min = Comment.builder()
-                .author("min")
-                .authorAvatarUrl("http://findicons.com/files/icons/1072/face_avatars/300/i03.png")
-                .date(LocalDateTime.now().plusSeconds(45))
-                .text("Example comment so lorem ipsum here will be nice")
-                .build();
-
-        Comment min2 = Comment.builder()
-                .author("min2")
-                .authorAvatarUrl("http://findicons.com/files/icons/1072/face_avatars/300/i03.png")
-                .date(LocalDateTime.now().plusSeconds(65))
-                .text("Example comment so lorem ipsum here will be nice")
-                .build();
-
-        Comment fewMin = Comment.builder()
-                .author("fewMin")
-                .authorAvatarUrl("https://cdn0.iconfinder.com/data/icons/iconshock_guys/512/matthew.png")
-                .date(LocalDateTime.now().plusMinutes(3))
-                .text("Example comment so lorem ipsum here will be nice. Something new here will be nice")
-                .build();
-
-        Comment hour = Comment.builder()
-                .author("hour")
-                .authorAvatarUrl("https://cdn0.iconfinder.com/data/icons/iconshock_guys/512/matthew.png")
-                .date(LocalDateTime.now().plusMinutes(60 - 15))
-                .text("Example comment so lorem ipsum here will be nice. Something new here will be nice")
-                .build();
-
-        Comment hour2 = Comment.builder()
-                .author("hour2")
-                .authorAvatarUrl("https://cdn0.iconfinder.com/data/icons/iconshock_guys/512/matthew.png")
-                .date(LocalDateTime.now().plusMinutes(60))
-                .text("Example comment so lorem ipsum here will be nice. Something new here will be nice")
-                .build();
-
-        Comment hour3 = Comment.builder()
-                .author("hour3")
-                .authorAvatarUrl("https://cdn0.iconfinder.com/data/icons/iconshock_guys/512/matthew.png")
-                .date(LocalDateTime.now().plusMinutes(60 + 15))
-                .text("Example comment so lorem ipsum here will be nice. Something new here will be nice")
-                .build();
-
-        Comment fewH = Comment.builder()
-                .author("fewH =")
-                .authorAvatarUrl("https://cdn0.iconfinder.com/data/icons/iconshock_guys/512/matthew.png")
-                .date(LocalDateTime.now().plusHours(12))
-                .text("Example comment so lorem ipsum here will be nice. Something new here will be nice")
-                .build();
-
-        Comment yesterday = Comment.builder()
-                .author("yesterday")
-                .authorAvatarUrl("https://cdn0.iconfinder.com/data/icons/iconshock_guys/512/matthew.png")
-                .date(LocalDateTime.now().plusHours(25))
-                .text("Example comment so lorem ipsum here will be nice. Something new here will be nice")
-                .build();
-
-        Comment date = Comment.builder()
-                .author("date")
-                .authorAvatarUrl("https://cdn0.iconfinder.com/data/icons/iconshock_guys/512/matthew.png")
-                .date(LocalDateTime.now().plusHours(48))
-                .text("Example comment so lorem ipsum here will be nice. Something new here will be nice")
-                .build();
-
-        comments.add(sec);
-        comments.add(fewSec);
-        comments.add(min);
-        comments.add(min2);
-        comments.add(fewMin);
-        comments.add(hour);
-        comments.add(hour2);
-        comments.add(hour3);
-        comments.add(fewH);
-        comments.add(yesterday);
-        comments.add(date);
-        ShotDetails details = ShotDetails
-                .builder()
-                .id(1)
-                .title("Awsome Title homie")
-                .comments(comments)
-                .userAvatarUrl("https://d13yacurqjgara.cloudfront.net/users/653174/avatars/normal/4765adea2b386b03231d10f37d786f8e.jpg?1475482306")
-                .authorUrl("https://d13yacurqjgara.cloudfront.net/users/653174/avatars/normal/4765adea2b386b03231d10f37d786f8e.jpg?1475482306")
-                .authorName("demo author")
-                .appName("demo app")
-                .bucketCount(123)
-                .likesCount(321)
-                .companyName("Netguru dmeos")
-                .companyProfileUrl("http://google.com")
-                .date("25 dev 2016")
-                .authorId(99)
-                .description("\"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\"")
-                .build();
-//
-        showItems(details);
-        showMainImage("https://d13yacurqjgara.cloudfront.net/users/653174/avatars/normal/4765adea2b386b03231d10f37d786f8e.jpg?1475482306",
-                "https://d13yacurqjgara.cloudfront.net/users/653174/avatars/normal/4765adea2b386b03231d10f37d786f8e.jpg?1475482306");
+        // TODO: 15.11.2016 MOCKED DATA - remove in task IA-146
+        showItems(MockedExampleData.getMocketShotDetailsData());
+        showMainImage(MockedExampleData.getExampleImageUrl(), MockedExampleData.getExampleImageUrl());
+        // TODO: 15.11.2016 MOCKED DATA - remove in task IA-146
     }
 
     private void initRecycler() {
