f446165e08579c5869835ae50d3b6a3b85a40a79
Rafal Adasiewicz <rafal.adasiewicz@netguru.pl>
2016-11-15 14:31:27 +0100

    [IA-32] Review fix

--- app/src/main/java/co/netguru/android/inbbbox/feature/followers/details/adapter/FollowerDetailsAdapter.java
+++ app/src/main/java/co/netguru/android/inbbbox/feature/followers/details/adapter/FollowerDetailsAdapter.java
@@ -51,6 +51,7 @@ public class FollowerDetailsAdapter extends RecyclerView.Adapter<RecyclerView.Vi
                 break;
             case TYPE_LIST:
                 ((FollowerDetailsListViewHolder) holder).bind(shotList.get(position - 1));
+                break;
             default:
                 throw new IllegalArgumentException("There is no item type for holder on this position : " + position);
         }
