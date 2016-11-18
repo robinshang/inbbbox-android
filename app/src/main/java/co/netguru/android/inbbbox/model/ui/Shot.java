30227f3539f59cf1be82e0a391b107d62cd56abe
mmarkiew <maciej.markiew@gmail.com>
2016-11-18 15:01:58 +0100

    [IA-144] like bucket state marked as nullable

--- app/src/main/java/co/netguru/android/inbbbox/model/ui/Shot.java
+++ app/src/main/java/co/netguru/android/inbbbox/model/ui/Shot.java
@@ -50,9 +50,11 @@ public abstract class Shot implements Parcelable, ShotImage {
     @Nullable
     public abstract String thumbnailUrl();
 
-    public abstract boolean isBucketed();
+    @Nullable
+    public abstract Boolean isBucketed();
 
-    public abstract boolean isLiked();
+    @Nullable
+    public abstract Boolean isLiked();
 
     @AutoValue.Builder
     public abstract static class Builder {
@@ -72,9 +74,9 @@ public abstract class Shot implements Parcelable, ShotImage {
 
         public abstract Shot.Builder description(String description);
 
-        public abstract Shot.Builder isLiked(boolean state);
+        public abstract Shot.Builder isLiked(Boolean state);
 
-        public abstract Shot.Builder isBucketed(boolean state);
+        public abstract Shot.Builder isBucketed(Boolean state);
 
         public abstract Shot.Builder isGif(boolean state);
 
@@ -102,9 +104,6 @@ public abstract class Shot implements Parcelable, ShotImage {
                 .team(shotEntity.getTeam() != null ? Team.create(shotEntity.getTeam()) : null)
                 .bucketCount(shotEntity.getBucketsCount())
                 .likesCount(shotEntity.getLikesCount())
-                // TODO: 17.11.2016
-                .isBucketed(false)
-                .isLiked(false)
                 .date(shotEntity.getCreatedAt())
                 .isGif(shotEntity.getAnimated())
                 .hdpiImageUrl(shotEntity.getImage().hiDpiUrl())
