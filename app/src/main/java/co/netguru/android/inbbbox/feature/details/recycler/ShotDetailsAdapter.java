bd9ff253374c6bd838d7b33e1891651a8042d892
mmarkiew <maciej.markiew@gmail.com>
2016-11-29 12:08:11 +0100

    [IA-22] adapter refactor

--- app/src/main/java/co/netguru/android/inbbbox/feature/details/recycler/ShotDetailsAdapter.java
+++ app/src/main/java/co/netguru/android/inbbbox/feature/details/recycler/ShotDetailsAdapter.java
@@ -13,8 +13,11 @@ import co.netguru.android.inbbbox.model.ui.Shot;
 
 public class ShotDetailsAdapter extends RecyclerView.Adapter<ShotDetailsViewHolder> {
 
-    static final int STATIC_ITEMS_COUNT = 2;
     private static final int LOAD_MORE_ITEM = 1;
+    private static final int LOAD_MORE_VIEW_TYPE = -99;
+    private static final int USER_INFO_VIEW_TYPE = 0;
+    private static final int DESCRIPTION_VIEW_TYPE = 1;
+    private static final int STATIC_ITEMS_COUNT = 2;
 
     private final DetailsViewActionCallback actionCallback;
     private Shot details;
@@ -28,18 +31,36 @@ public class ShotDetailsAdapter extends RecyclerView.Adapter<ShotDetailsViewHold
 
     @Override
     public ShotDetailsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return ShotDetailsViewFactory
-                .getViewHolder(viewType, parent, actionCallback);
+        ShotDetailsViewHolder viewHolder;
+        switch (viewType) {
+            case USER_INFO_VIEW_TYPE:
+                viewHolder = new ShotDetailsUserInfoViewHolder(parent, actionCallback);
+                break;
+            case DESCRIPTION_VIEW_TYPE:
+                viewHolder = new ShotDetailsDescriptionViewHolder(parent, actionCallback);
+                break;
+            case LOAD_MORE_VIEW_TYPE:
+                viewHolder = new ShotDetailsLoadMoreViewHolder(parent, actionCallback);
+                break;
+            default:
+                viewHolder = new ShotDetailsCommentViewHolder(parent, actionCallback);
+                break;
+        }
+        return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(ShotDetailsViewHolder holder, int position) {
-        holder.bind(details, comments);
+        Comment comment = null;
+        if (!comments.isEmpty() && position > STATIC_ITEMS_COUNT - 1) {
+            comment = comments.get(position - ShotDetailsAdapter.STATIC_ITEMS_COUNT);
+        }
+        holder.bind(details, comment);
     }
 
     @Override
     public int getItemViewType(int position) {
-        return position == getItemCount() - 1 ? ShotDetailsViewFactory.LOAD_MORE_ITEM_TYPE : position;
+        return position == getItemCount() - 1 ? LOAD_MORE_VIEW_TYPE : position;
     }
 
     @Override
--- app/src/main/java/co/netguru/android/inbbbox/feature/details/recycler/ShotDetailsLoadMoreViewHolder.java
+++ app/src/main/java/co/netguru/android/inbbbox/feature/details/recycler/ShotDetailsLoadMoreViewHolder.java
@@ -1,6 +1,9 @@
 package co.netguru.android.inbbbox.feature.details.recycler;
 
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -8,6 +11,8 @@ import butterknife.BindString;
 import butterknife.BindView;
 import butterknife.OnClick;
 import co.netguru.android.inbbbox.R;
+import co.netguru.android.inbbbox.model.ui.Comment;
+import co.netguru.android.inbbbox.model.ui.Shot;
 
 class ShotDetailsLoadMoreViewHolder extends ShotDetailsViewHolder {
 
@@ -22,8 +27,11 @@ class ShotDetailsLoadMoreViewHolder extends ShotDetailsViewHolder {
 
     private boolean isLoadingActive;
 
-    ShotDetailsLoadMoreViewHolder(View view, DetailsViewActionCallback actionCallback) {
-        super(view, actionCallback);
+    ShotDetailsLoadMoreViewHolder(ViewGroup parent, DetailsViewActionCallback actionCallback) {
+        super(LayoutInflater
+                        .from(parent.getContext())
+                        .inflate(R.layout.item_shot_load_more_layout, parent, false),
+                actionCallback);
     }
 
     @OnClick(R.id.shot_details_load_more_textView)
@@ -31,14 +39,6 @@ class ShotDetailsLoadMoreViewHolder extends ShotDetailsViewHolder {
         loadMoreProgressBar.setVisibility(View.VISIBLE);
         actionCallbackListener.onLoadMoreCommentsSelected();
         isLoadingActive = false;
-        updateButtonState();
-    }
-
-    @Override
-    protected void handleBinding() {
-        // TODO: 25.11.2016 handle loader initial state - in logic task
-        isLoadingActive = commentList != null && commentList.size() < item.commentsCount();
-        loadMoreProgressBar.setVisibility(View.GONE);
         updateButtonState();
     }
 
@@ -52,5 +52,13 @@ class ShotDetailsLoadMoreViewHolder extends ShotDetailsViewHolder {
         } else {
             loadMoreTextView.setText("");
         }
+    }
+
+    @Override
+    public void bind(@Nullable Shot item, @Nullable Comment comments) {
+        // TODO: 25.11.2016 handle loader initial state - in logic task
+        isLoadingActive = false;
+        loadMoreProgressBar.setVisibility(View.GONE);
+        updateButtonState();
     }
 }
