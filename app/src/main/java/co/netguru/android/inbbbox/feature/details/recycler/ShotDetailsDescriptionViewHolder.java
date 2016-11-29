6e3f4f9997b271805f26e583ea309ecb7f391cbc
mmarkiew <maciej.markiew@gmail.com>
2016-11-29 11:42:46 +0100

    [IA-22] description viewHolder

--- app/src/main/java/co/netguru/android/inbbbox/feature/details/recycler/ShotDetailsDescriptionViewHolder.java
+++ app/src/main/java/co/netguru/android/inbbbox/feature/details/recycler/ShotDetailsDescriptionViewHolder.java
@@ -1,11 +1,15 @@
 package co.netguru.android.inbbbox.feature.details.recycler;
 
+import android.support.annotation.Nullable;
 import android.text.method.LinkMovementMethod;
-import android.view.View;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import butterknife.BindView;
 import co.netguru.android.inbbbox.R;
+import co.netguru.android.inbbbox.model.ui.Comment;
+import co.netguru.android.inbbbox.model.ui.Shot;
 
 import static co.netguru.android.inbbbox.utils.StringUtils.getParsedHtmlTextSpanned;
 
@@ -14,12 +18,15 @@ class ShotDetailsDescriptionViewHolder extends ShotDetailsViewHolder {
     @BindView(R.id.shot_details_description_textView)
     TextView descriptionTextView;
 
-    ShotDetailsDescriptionViewHolder(View view, DetailsViewActionCallback actionCallback) {
-        super(view, actionCallback);
+    ShotDetailsDescriptionViewHolder(ViewGroup parent, DetailsViewActionCallback actionCallback) {
+        super(LayoutInflater
+                .from(parent.getContext())
+                .inflate(R.layout.item_shot_info_description_layout, parent, false),
+                actionCallback);
     }
 
     @Override
-    protected void handleBinding() {
+    public void bind(@Nullable Shot item, @Nullable Comment comments) {
         if (item.description() != null) {
             descriptionTextView.setText(getParsedHtmlTextSpanned(item.description()));
             descriptionTextView.setMovementMethod(LinkMovementMethod.getInstance());
