e82db74ce02f16d15e7b2f323eb7c5fb7839fa42
Rafal Adasiewicz <rafal.adasiewicz@netguru.pl>
2017-01-04 11:43:19 +0100

    [IA-196] Refactor likes, buckets and followers details

--- app/src/main/java/co/netguru/android/inbbbox/feature/follower/detail/adapter/FollowerDetailsGridViewHolder.java
+++ app/src/main/java/co/netguru/android/inbbbox/feature/follower/detail/adapter/FollowerDetailsGridViewHolder.java
@@ -1,37 +1,28 @@
 package co.netguru.android.inbbbox.feature.follower.detail.adapter;
 
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
 
-import butterknife.BindDimen;
 import butterknife.BindView;
 import butterknife.OnClick;
 import co.netguru.android.inbbbox.R;
-import co.netguru.android.inbbbox.common.utils.ShotLoadingUtil;
 import co.netguru.android.inbbbox.data.shot.model.ui.Shot;
 import co.netguru.android.inbbbox.feature.shared.ShotClickListener;
 import co.netguru.android.inbbbox.feature.shared.base.BaseViewHolder;
-import co.netguru.android.inbbbox.feature.shared.view.RoundedCornersImageView;
+import co.netguru.android.inbbbox.feature.shared.view.RoundedCornersShotImageView;
 
 public class FollowerDetailsGridViewHolder extends BaseViewHolder<Shot> {
 
-    private final ShotClickListener shotClickListener;
-
-    @BindDimen(R.dimen.shot_corner_radius)
-    float radius;
-
     @BindView(R.id.follower_details_item_image_view)
-    RoundedCornersImageView imageView;
+    RoundedCornersShotImageView shotImageView;
 
-    @BindView(R.id.gif_label_textView)
-    View gifLabelView;
+    private final ShotClickListener shotClickListener;
 
     private Shot item;
 
     FollowerDetailsGridViewHolder(ViewGroup parent, ShotClickListener shotClickListener) {
         super(LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.follower_detail_item_grid_view, parent, false));
+                .inflate(R.layout.follower_detail_item_view, parent, false));
         this.shotClickListener = shotClickListener;
     }
 
@@ -43,12 +34,6 @@ public class FollowerDetailsGridViewHolder extends BaseViewHolder<Shot> {
     @Override
     public void bind(Shot item) {
         this.item = item;
-        imageView.setRadius(radius);
-        if (item.isGif()) {
-            gifLabelView.setVisibility(View.VISIBLE);
-        } else {
-            gifLabelView.setVisibility(View.GONE);
-        }
-        ShotLoadingUtil.loadListShot(itemView.getContext(), imageView, item);
+        shotImageView.loadShot(item);
     }
 }
