20d385b40451c08e1cb8c7a67e72284aba7434bf
mmarkiew <maciej.markiew@gmail.com>
2017-01-31 14:14:12 +0100

    [IA-149] bucket shots items list adjustment to landscape  view

--- app/src/main/java/co/netguru/android/inbbbox/feature/shared/base/bottomsheet/HidingBottomSheetActivityDelegate.java
+++ app/src/main/java/co/netguru/android/inbbbox/feature/shared/base/bottomsheet/HidingBottomSheetActivityDelegate.java
@@ -68,7 +68,6 @@ public class HidingBottomSheetActivityDelegate implements HidingBottomSheetBeare
             @Override
             public void onSlide(@NonNull View bottomSheet, float slideOffset) {
                 bottomSheetActivityCallback.onBottomSheetSlide(slideOffset);
-                //no-op
             }
         };
     }
