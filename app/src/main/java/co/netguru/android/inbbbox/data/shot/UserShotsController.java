1a41a038137ba76fcf6b933ba89bb5148296410b
Joanna Jasnowska <joanna.jasnowska@netguru.co>
2017-02-13 00:29:50 +0100

    [IA-288] Add method to get team as user with shots

--- app/src/main/java/co/netguru/android/inbbbox/data/shot/UserShotsController.java
+++ app/src/main/java/co/netguru/android/inbbbox/data/shot/UserShotsController.java
@@ -5,8 +5,13 @@ import java.util.List;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
+import co.netguru.android.inbbbox.data.dribbbleuser.team.Team;
+import co.netguru.android.inbbbox.data.dribbbleuser.user.User;
+import co.netguru.android.inbbbox.data.follower.model.ui.UserWithShots;
 import co.netguru.android.inbbbox.data.shot.model.ui.Shot;
 import rx.Observable;
+
+import static co.netguru.android.commons.rx.RxTransformers.fromListObservable;
 
 @Singleton
 public class UserShotsController {
@@ -23,5 +28,13 @@ public class UserShotsController {
                 .flatMap(Observable::from)
                 .map(Shot::create)
                 .toList();
+    }
+
+    public Observable<UserWithShots> getTeamUserWithShots(Team team, int pageNumber, int pageCount) {
+        return getUserShotsList(team.id(), pageNumber, pageCount)
+                .compose(fromListObservable())
+                .map(shot -> Shot.update(shot).author(User.createFromTeam(team)).build())
+                .toList()
+                .map(shotList -> UserWithShots.create(User.createFromTeam(team), shotList));
     }
 }
