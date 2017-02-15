985b4b9ec15452ad483f07656b37b14509b2aed8
lukaszjanyga <alvarglj@gmail.com>
2017-02-15 15:54:29 +0100

    replace white corner overlay with clipping path

--- app/src/main/java/co/netguru/android/inbbbox/feature/shared/view/RoundedCornersView.java
+++ app/src/main/java/co/netguru/android/inbbbox/feature/shared/view/RoundedCornersView.java
@@ -5,7 +5,9 @@ import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.DrawFilter;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
 import android.graphics.RectF;
@@ -28,12 +30,10 @@ public abstract class RoundedCornersView extends FrameLayout {
     @BindColor(R.color.windowBackground)
     int defaultRoundedCornersBackground;
 
-    @ColorInt
-    private int roundedCornersBackgroundColor;
     private boolean isRoundingBottomCornersEnabled;
-    private Bitmap maskBitmap;
-    private Paint maskPaint;
-    private Paint cornersMaskPaint;
+
+    private Path clipPath = new Path();
+    private RectF clippingRect;
 
     public RoundedCornersView(Context context) {
         super(context);
@@ -53,41 +53,20 @@ public abstract class RoundedCornersView extends FrameLayout {
     }
 
     @Override
-    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        super.onSizeChanged(w, h, oldw, oldh);
-        if (w != oldw || h != oldh) {
-            maskBitmap = createMask(w, h);
-        }
-    }
-
-    @Override
     public void draw(Canvas canvas) {
+        if (clippingRect == null) {
+            initClippingPath();
+        }
+        if (isRoundingBottomCornersEnabled)
+            canvas.clipPath(clipPath);
         super.draw(canvas);
-        canvas.drawBitmap(maskBitmap, 0f, 0f, maskPaint);
     }
 
     @LayoutRes
     protected abstract int getLayoutResource();
 
-    private Bitmap createMask(int width, int height) {
-        final Bitmap mask = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
-        final Canvas canvas = new Canvas(mask);
-
-        mask.eraseColor(Color.TRANSPARENT);
-        canvas.drawColor(roundedCornersBackgroundColor);
-        canvas.drawRoundRect(new RectF(0, 0, width, height), radius, radius, cornersMaskPaint);
-
-        if (!isRoundingBottomCornersEnabled) {
-            canvas.drawRect(0, height - radius, width, height, cornersMaskPaint);
-        }
-
-        return mask;
-    }
-
     private void handleAttrs(Context context, AttributeSet attrs) {
         final TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.RoundedCornersView);
-        roundedCornersBackgroundColor = a.getColor(R.styleable.RoundedCornersView_roundedCornersBackgroundColor,
-                defaultRoundedCornersBackground);
         isRoundingBottomCornersEnabled = a.getBoolean(R.styleable.RoundedCornersView_roundingBottomCornersEnabled,
                 true);
         a.recycle();
@@ -96,10 +75,11 @@ public abstract class RoundedCornersView extends FrameLayout {
     private void init() {
         LayoutInflater.from(getContext()).inflate(getLayoutResource(), this);
         ButterKnife.bind(this);
-        maskPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        cornersMaskPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        cornersMaskPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
-
         setWillNotDraw(false);
+    }
+
+    private void initClippingPath() {
+        clippingRect = new RectF(0, 0, getWidth(), getHeight());
+        clipPath.addRoundRect(clippingRect, radius, radius, Path.Direction.CW);
     }
 }
