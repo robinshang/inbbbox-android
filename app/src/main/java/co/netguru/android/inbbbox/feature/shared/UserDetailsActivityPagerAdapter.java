a1934abacf841476c660d5e30f337e7f2f990675
Joanna Jasnowska <joanna.jasnowska@netguru.co>
2017-02-20 14:01:11 +0100

    [IA-300] Add viewPager to the team view

---
+++ app/src/main/java/co/netguru/android/inbbbox/feature/shared/UserDetailsActivityPagerAdapter.java
@@ -0,0 +1,78 @@
+package co.netguru.android.inbbbox.feature.shared;
+
+import android.annotation.SuppressLint;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.util.SparseArray;
+import android.view.ViewGroup;
+
+import co.netguru.android.inbbbox.feature.bucket.BucketsFragment;
+import co.netguru.android.inbbbox.feature.main.adapter.RefreshableFragment;
+import co.netguru.android.inbbbox.feature.shot.ShotsFragment;
+
+public class UserDetailsActivityPagerAdapter<T extends Fragment & RefreshableFragment>
+        extends FragmentStatePagerAdapter {
+
+    private SparseArray<T> activeRefreshableFragments;
+
+    public UserDetailsActivityPagerAdapter(FragmentManager fm) {
+        super(fm);
+        activeRefreshableFragments = new SparseArray<>(UserDetailsTabItemType.values().length);
+    }
+
+    @SuppressLint("DefaultLocale")
+    @Override
+    public Fragment getItem(int position) {
+        Fragment result;
+
+        switch (UserDetailsTabItemType.getTabItemForPosition(position)) {
+            case SHOTS:
+//               TODO: 20.02 Instantiate ShotsFragment [not in scope of this task]
+                result = ShotsFragment.newInstance();
+                break;
+            case INFO:
+//                 TODO: 20.02 Instantiate InfoFragment [not in scope of this task]
+                result = ShotsFragment.newInstance();
+                break;
+            case PROJECTS:
+//                 TODO: 20.02 Instantiate ProjectsFragment [not in scope of this task]
+                result = ShotsFragment.newInstance();
+                break;
+            case BUCKETS:
+//                TODO: 20.02 Instantiate BucketsFragment [not in scope of this task]
+                result = BucketsFragment.newInstance();
+                break;
+            default:
+                throw new IllegalArgumentException(String.format(
+                        "There is no fragment defined for position: %d", position));
+        }
+        return result;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        //noinspection unchecked
+        T refreshableFragment = (T) super.instantiateItem(container, position);
+        activeRefreshableFragments.put(position, refreshableFragment);
+        return refreshableFragment;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        activeRefreshableFragments.delete(position);
+        super.destroyItem(container, position, object);
+    }
+
+    @Override
+    public int getCount() {
+        return co.netguru.android.inbbbox.feature.main.adapter.TabItemType.values().length;
+    }
+
+    public void refreshFragment(co.netguru.android.inbbbox.feature.main.adapter.TabItemType tabItemType) {
+        T refreshableFragment = activeRefreshableFragments.get(tabItemType.getPosition());
+        if (refreshableFragment != null) {
+            refreshableFragment.refreshFragmentData();
+        }
+    }
+}
--- app/src/main/java/co/netguru/android/inbbbox/feature/team/TeamDetailsActivity.java
+++ app/src/main/java/co/netguru/android/inbbbox/feature/team/TeamDetailsActivity.java
@@ -2,28 +2,55 @@ package co.netguru.android.inbbbox.feature.team;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.List;
+
 import butterknife.BindColor;
+import butterknife.BindDrawable;
 import butterknife.BindView;
+import co.netguru.android.inbbbox.Constants;
 import co.netguru.android.inbbbox.R;
 import co.netguru.android.inbbbox.data.follower.model.ui.UserWithShots;
+import co.netguru.android.inbbbox.data.shot.model.ui.Shot;
+import co.netguru.android.inbbbox.feature.shared.UserDetailsActivityPagerAdapter;
+import co.netguru.android.inbbbox.feature.shared.UserDetailsTabItemType;
 import co.netguru.android.inbbbox.feature.shared.base.BaseActivity;
+import co.netguru.android.inbbbox.feature.shared.view.NonSwipeableViewPager;
+import co.netguru.android.inbbbox.feature.shot.ShotsFragment;
+import co.netguru.android.inbbbox.feature.shot.detail.ShotDetailsRequest;
 
-public class TeamDetailsActivity extends BaseActivity {
+public class TeamDetailsActivity extends BaseActivity implements ShotsFragment.ShotActionListener {
 
     private static final String USER_KEY = "user_key";
 
-    @BindView(R.id.toolbar)
-    Toolbar toolbar;
-
     @BindColor(R.color.white)
     int colorWhite;
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.user_details_tab_layout)
+    TabLayout tabLayout;
+    @BindView(R.id.user_details_view_pager)
+    NonSwipeableViewPager viewPager;
+
+    @BindDrawable(R.drawable.toolbar_center_background)
+    Drawable toolbarCenterBackground;
+    @BindDrawable(R.drawable.toolbar_start_background)
+    Drawable toolbarStartBackground;
+
+    @BindColor(R.color.accent)
+    int highlightColor;
+
+    private int currentTabIndex = Constants.UNDEFINED;
+    private UserDetailsActivityPagerAdapter pagerAdapter;
 
     public static void startActivity(Context context, UserWithShots user) {
         final Intent intent = new Intent(context, TeamDetailsActivity.class);
@@ -34,12 +61,9 @@ public class TeamDetailsActivity extends BaseActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_follower_details);
+        setContentView(R.layout.activity_user_details);
         initializeToolbar();
-
-        if (savedInstanceState == null) {
-            instantiateFragment();
-        }
+        initializePager();
     }
 
     @Override
@@ -59,10 +83,16 @@ public class TeamDetailsActivity extends BaseActivity {
         }
     }
 
-    private void instantiateFragment() {
-        replaceFragment(R.id.follower_details_fragment_container,
-                TeamDetailsFragment.newInstance(getIntent().getParcelableExtra(USER_KEY)),
-                TeamDetailsFragment.TAG).commit();
+    public void initializePager() {
+        pagerAdapter = new UserDetailsActivityPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(pagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+
+        for (final UserDetailsTabItemType item : UserDetailsTabItemType.values()) {
+            final TabLayout.Tab tab = tabLayout.getTabAt(item.getPosition());
+            selectInitialTabSelection(tab, item.getPosition());
+        }
+        tabLayout.addOnTabSelectedListener(createTabListener());
     }
 
     private void initializeToolbar() {
@@ -77,4 +107,52 @@ public class TeamDetailsActivity extends BaseActivity {
         }
     }
 
+    private void selectInitialTabSelection(TabLayout.Tab tab, int position) {
+        if (position == 0) {
+            selectTab(tab);
+        }
+    }
+
+    private TabLayout.OnTabSelectedListener createTabListener() {
+        return new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                selectTab(tab);
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                //no op
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                selectTab(tab);
+            }
+        };
+    }
+
+    private void selectTab(TabLayout.Tab tab) {
+        currentTabIndex = tab.getPosition();
+
+        tab.setText(getString(UserDetailsTabItemType.getTabItemForPosition(tab.getPosition()).getTitle()));
+        setupToolbarForCurrentTab(currentTabIndex);
+    }
+
+    private void setupToolbarForCurrentTab(int position) {
+        toolbar.setBackground(position == UserDetailsTabItemType.SHOTS.getPosition()
+                ? toolbarCenterBackground : toolbarStartBackground);
+    }
+
+    @Override
+    public void shotLikeStatusChanged() {
+
+    }
+
+    @Override
+    public void showShotDetails(Shot shot, List<Shot> nearbyShots, ShotDetailsRequest detailsRequest) {
+
+    }
 }
