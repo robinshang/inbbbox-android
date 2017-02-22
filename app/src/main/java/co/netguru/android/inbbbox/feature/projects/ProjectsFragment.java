364310038577f3749840f770ec22ecc33efed146
Rafal Adasiewicz <rafal.adasiewicz@netguru.pl>
2017-02-22 13:43:36 +0100

    [IA-313] WIP

--- app/src/main/java/co/netguru/android/inbbbox/feature/projects/ProjectsFragment.java
+++ app/src/main/java/co/netguru/android/inbbbox/feature/projects/ProjectsFragment.java
@@ -3,16 +3,33 @@ package co.netguru.android.inbbbox.feature.projects;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import butterknife.BindColor;
+import butterknife.BindView;
 import co.netguru.android.inbbbox.R;
 import co.netguru.android.inbbbox.app.App;
+import co.netguru.android.inbbbox.feature.projects.adapter.ProjectsAdapter;
 import co.netguru.android.inbbbox.feature.shared.base.BaseMvpFragment;
 
 public class ProjectsFragment extends BaseMvpFragment<ProjectsContract.View, ProjectsContract.Presenter>
         implements ProjectsContract.View {
+
+    @BindView(R.id.contentView)
+    SwipeRefreshLayout swipeRefreshLayout;
+    @BindView(R.id.projects_recycler_view)
+    RecyclerView recyclerView;
+
+    @BindColor(R.color.accent)
+    int accentColor;
+
+    private ProjectsAdapter projectsAdapter;
+    private LinearLayoutManager linearLayoutManager;
 
     public static ProjectsFragment newInstance() {
         return new ProjectsFragment();
@@ -21,13 +38,35 @@ public class ProjectsFragment extends BaseMvpFragment<ProjectsContract.View, Pro
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        // TODO: 22.02.2017 Create proper layout
-        return inflater.inflate(R.layout.fragment_buckets, container, false);
+        return inflater.inflate(R.layout.fragment_projects, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initRecyclerView();
+        initSwipeRefreshView();
     }
 
     @NonNull
     @Override
     public ProjectsContract.Presenter createPresenter() {
         return App.getUserComponent(getContext()).plusProjectsComponent().getPresenter();
+    }
+
+    private void initSwipeRefreshView() {
+        swipeRefreshLayout.setColorSchemeColors(accentColor);
+        swipeRefreshLayout.setOnRefreshListener(() -> {
+            // TODO: 22.02.2017 Call proper method from presenter
+        });
+    }
+
+    private void initRecyclerView() {
+        projectsAdapter = new ProjectsAdapter();
+        linearLayoutManager = new LinearLayoutManager(getContext());
+
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(projectsAdapter);
     }
 }
---
+++ app/src/main/java/co/netguru/android/inbbbox/feature/projects/adapter/ProjectsAdapter.java
@@ -0,0 +1,23 @@
+package co.netguru.android.inbbbox.feature.projects.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+public class ProjectsAdapter extends RecyclerView.Adapter<Object> {
+
+
+    @Override
+    public Object onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(Object holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
