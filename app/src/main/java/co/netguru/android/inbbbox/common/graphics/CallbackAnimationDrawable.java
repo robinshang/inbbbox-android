94886b95460da0f60f4710fd7719b452bd40af6b
Gonzalo Acosta <acosta.gonzalo@gmail.com>
2017-03-08 11:54:39 +0100

    Try to change animation placeholder for gif when is load
    [IA-170]

---
+++ app/src/main/java/co/netguru/android/inbbbox/common/graphics/CallbackAnimationDrawable.java
@@ -0,0 +1,76 @@
+package co.netguru.android.inbbbox.common.graphics;
+
+
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.Drawable.Callback;
+import android.view.View;
+
+/**
+ * Provides a callback when a non-looping {@link AnimationDrawable} completes its animation sequence. More precisely,
+ * {@link #onAnimationComplete()} is triggered when {@link View#invalidateDrawable(Drawable)} has been called on the
+ * last frame.
+ *
+ * @author Benedict Lau
+ */
+public abstract class CallbackAnimationDrawable implements Callback {
+
+    /**
+     * The last frame of {@link Drawable} in the {@link AnimationDrawable}.
+     */
+    private Drawable mLastFrame;
+
+    /**
+     * The client's {@link Callback} implementation. All calls are proxied to this wrapped {@link Callback}
+     * implementation after intercepting the events we need.
+     */
+    private Callback mWrappedCallback;
+
+    /**
+     * Flag to ensure that {@link #onAnimationComplete()} is called only once, since
+     * {@link #invalidateDrawable(Drawable)} may be called multiple times.
+     */
+    private boolean mIsCallbackTriggered = false;
+
+    /**
+     * @param animationDrawable the {@link AnimationDrawable}.
+     * @param callback          the client's {@link Callback} implementation. This is usually the {@link View} the has the
+     *                          {@link AnimationDrawable} as background.
+     */
+    public CallbackAnimationDrawable(AnimationDrawable animationDrawable, Callback callback) {
+        mLastFrame = animationDrawable.getFrame(animationDrawable.getNumberOfFrames() - 1);
+        mWrappedCallback = callback;
+    }
+
+    @Override
+    public void invalidateDrawable(Drawable who) {
+        if (mWrappedCallback != null) {
+            mWrappedCallback.invalidateDrawable(who);
+        }
+
+        if (!mIsCallbackTriggered && mLastFrame != null && mLastFrame.equals(who.getCurrent())) {
+            mIsCallbackTriggered = true;
+            onAnimationComplete();
+        }
+    }
+
+    @Override
+    public void scheduleDrawable(Drawable who, Runnable what, long when) {
+        if (mWrappedCallback != null) {
+            mWrappedCallback.scheduleDrawable(who, what, when);
+        }
+    }
+
+    @Override
+    public void unscheduleDrawable(Drawable who, Runnable what) {
+        if (mWrappedCallback != null) {
+            mWrappedCallback.unscheduleDrawable(who, what);
+        }
+    }
+
+    /**
+     * Callback triggered when {@link View#invalidateDrawable(Drawable)} has been called on the last frame, which marks
+     * the end of a non-looping animation sequence.
+     */
+    public abstract void onAnimationComplete();
+}
--- app/src/main/java/co/netguru/android/inbbbox/common/utils/ShotLoadingUtil.java
+++ app/src/main/java/co/netguru/android/inbbbox/common/utils/ShotLoadingUtil.java
@@ -1,6 +1,9 @@
 package co.netguru.android.inbbbox.common.utils;
 
 import android.content.Context;
+import android.graphics.drawable.AnimationDrawable;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.widget.ImageView;
 
 import com.bumptech.glide.DrawableTypeRequest;
@@ -8,13 +11,17 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.drawable.GlideDrawable;
 import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
+import com.bumptech.glide.request.target.Target;
 
 import co.netguru.android.inbbbox.R;
+import co.netguru.android.inbbbox.common.graphics.CallbackAnimationDrawable;
 import co.netguru.android.inbbbox.data.shot.model.ui.ShotImage;
 import jp.wasabeef.glide.transformations.BlurTransformation;
 import timber.log.Timber;
 
 public class ShotLoadingUtil {
+
+    private final static String GIF_EXTENSION = ".gif";
 
     private ShotLoadingUtil() {
         throw new AssertionError();
@@ -53,26 +60,43 @@ public class ShotLoadingUtil {
     }
 
     public static void loadMainViewShot(Context context, ImageView target, ShotImage shot) {
-        loadMainViewShotWithListener(context, target, shot, null);
+        loadMainViewShotWithListener(context, target, shot, getRequestListener(shot, target));
     }
 
     public static void loadMainViewShotWithListener(Context context, ImageView target, ShotImage shot,
                                                     RequestListener<String, GlideDrawable> requestListener) {
         String imageUrl = getImageUrl(shot);
         Timber.d("shot image url: %s", imageUrl);
-        GlideDrawableImageViewTarget imageViewTarget = new GlideDrawableImageViewTarget(target);
-        Glide.with(context)
-                .load(imageUrl)
-                .listener(requestListener)
-                .placeholder(R.drawable.shot_placeholder)
-                .thumbnail(ShotLoadingUtil.getThumbnailRequest(context, shot.thumbnailUrl()))
-                .animate(android.R.anim.fade_in)
-                .into(imageViewTarget);
+
+        final boolean isGif = isGif(imageUrl);
+        if (isGif) {
+            target.setBackgroundColor(ContextCompat.getColor(context, R.color.white));
+            target.setBackgroundResource(R.drawable.basketball_loader);
+            ((AnimationDrawable) target.getBackground()).start();
+
+            GlideDrawableImageViewTarget imageViewTarget = new GlideDrawableImageViewTarget(target);
+            Glide.with(context)
+                    .load(imageUrl)
+                    .listener(requestListener)
+                    .error(R.drawable.logo_empty)
+                    .animate(android.R.anim.fade_in)
+                    .into(imageViewTarget);
+
+        } else {
+            GlideDrawableImageViewTarget imageViewTarget = new GlideDrawableImageViewTarget(target);
+            Glide.with(context)
+                    .load(imageUrl)
+                    .listener(requestListener)
+                    .placeholder(R.drawable.shot_placeholder)
+                    .thumbnail(ShotLoadingUtil.getThumbnailRequest(context, shot.thumbnailUrl()))
+                    .animate(android.R.anim.fade_in)
+                    .into(imageViewTarget);
+        }
     }
 
     public static void loadMainViewShotNoPlaceholder(Context context,
-                                                                 ImageView target,
-                                                                 ShotImage shot) {
+                                                     ImageView target,
+                                                     ShotImage shot) {
 
         String imageUrl = getImageUrl(shot);
         Timber.d("shot image url: %s", imageUrl);
@@ -81,5 +105,40 @@ public class ShotLoadingUtil {
                 .load(imageUrl)
                 .animate(android.R.anim.fade_in)
                 .into(imageViewTarget);
+    }
+
+    private static boolean isGif(@NonNull String url) {
+        return url.endsWith(GIF_EXTENSION);
+    }
+
+    private static RequestListener<String, GlideDrawable> getRequestListener(ShotImage shotImage, ImageView targetImageView) {
+        if (isGif(getImageUrl(shotImage))) {
+            return new RequestListener<String, GlideDrawable>() {
+                @Override
+                public boolean onException(Exception e, String model, Target<GlideDrawable> target, boolean isFirstResource) {
+                    return false;
+                }
+
+                @Override
+                public boolean onResourceReady(GlideDrawable resource, String model, Target<GlideDrawable> target, boolean isFromMemoryCache, boolean isFirstResource) {
+                    AnimationDrawable animationDrawable = (AnimationDrawable) targetImageView.getBackground();
+                    animationDrawable.setCallback(stopAnimation(animationDrawable, resource, targetImageView));
+                    return true;
+                }
+            };
+        } else {
+            return null;
+        }
+    }
+
+    private static CallbackAnimationDrawable stopAnimation(final AnimationDrawable animationDrawable, GlideDrawable resourlce, ImageView target) {
+        return new CallbackAnimationDrawable(animationDrawable, target) {
+            @Override
+            public void onAnimationComplete() {
+                target.setImageDrawable(resourlce);
+                resourlce.start();
+
+            }
+        };
     }
 }
