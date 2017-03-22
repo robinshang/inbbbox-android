4bce26914c5420083d1d0a16bb6fac67a964f308
Rafal Adasiewicz <rafal.adasiewicz@netguru.pl>
2017-03-22 15:22:38 +0100

    [IA-350] Add Links table to database

--- app/src/main/java/co/netguru/android/inbbbox/data/db/UserDB.java
+++ app/src/main/java/co/netguru/android/inbbbox/data/db/UserDB.java
@@ -3,6 +3,8 @@ package co.netguru.android.inbbbox.data.db;
 import org.greenrobot.greendao.annotation.Entity;
 import org.greenrobot.greendao.annotation.Generated;
 import org.greenrobot.greendao.annotation.Id;
+import org.greenrobot.greendao.annotation.ToOne;
+import org.greenrobot.greendao.DaoException;
 
 @Entity
 public class UserDB {
@@ -20,11 +22,31 @@ public class UserDB {
     private String bio;
     private String location;
     private String type;
+    @ToOne(joinProperty = "id")
+    private LinksDB links;
+
+    /**
+     * Used to resolve relations
+     */
+    @Generated(hash = 2040040024)
+    private transient DaoSession daoSession;
+    /**
+     * Used for active entity operations.
+     */
+    @Generated(hash = 1450670558)
+    private transient UserDBDao myDao;
+    @Generated(hash = 1078680006)
+    private transient Long links__resolvedKey;
+
+
+    @Generated(hash = 1312299826)
+    public UserDB() {
+    }
 
     @Generated(hash = 1675469231)
-    public UserDB(Long id, String name, String avatarUrl, String username,
-                  int shotsCount, int bucketsCount, int projectsCount, int followersCount,
-                  int followingsCount, String bio, String location, String type) {
+    public UserDB(Long id, String name, String avatarUrl, String username, int shotsCount,
+                  int bucketsCount, int projectsCount, int followersCount, int followingsCount,
+                  String bio, String location, String type) {
         this.id = id;
         this.name = name;
         this.avatarUrl = avatarUrl;
@@ -37,10 +59,6 @@ public class UserDB {
         this.bio = bio;
         this.location = location;
         this.type = type;
-    }
-
-    @Generated(hash = 1312299826)
-    public UserDB() {
     }
 
     public Long getId() {
@@ -137,5 +155,81 @@ public class UserDB {
 
     public void setType(String type) {
         this.type = type;
+    }
+
+    /**
+     * To-one relationship, resolved on first access.
+     */
+    @Generated(hash = 225080737)
+    public LinksDB getLinks() {
+        Long __key = this.id;
+        if (links__resolvedKey == null || !links__resolvedKey.equals(__key)) {
+            final DaoSession daoSession = this.daoSession;
+            if (daoSession == null) {
+                throw new DaoException("Entity is detached from DAO context");
+            }
+            LinksDBDao targetDao = daoSession.getLinksDBDao();
+            LinksDB linksNew = targetDao.load(__key);
+            synchronized (this) {
+                links = linksNew;
+                links__resolvedKey = __key;
+            }
+        }
+        return links;
+    }
+
+    /**
+     * called by internal mechanisms, do not call yourself.
+     */
+    @Generated(hash = 1731259973)
+    public void setLinks(LinksDB links) {
+        synchronized (this) {
+            this.links = links;
+            id = links == null ? null : links.getId();
+            links__resolvedKey = id;
+        }
+    }
+
+    /**
+     * Convenient call for {@link org.greenrobot.greendao.AbstractDao#delete(Object)}.
+     * Entity must attached to an entity context.
+     */
+    @Generated(hash = 128553479)
+    public void delete() {
+        if (myDao == null) {
+            throw new DaoException("Entity is detached from DAO context");
+        }
+        myDao.delete(this);
+    }
+
+    /**
+     * Convenient call for {@link org.greenrobot.greendao.AbstractDao#refresh(Object)}.
+     * Entity must attached to an entity context.
+     */
+    @Generated(hash = 1942392019)
+    public void refresh() {
+        if (myDao == null) {
+            throw new DaoException("Entity is detached from DAO context");
+        }
+        myDao.refresh(this);
+    }
+
+    /**
+     * Convenient call for {@link org.greenrobot.greendao.AbstractDao#update(Object)}.
+     * Entity must attached to an entity context.
+     */
+    @Generated(hash = 713229351)
+    public void update() {
+        if (myDao == null) {
+            throw new DaoException("Entity is detached from DAO context");
+        }
+        myDao.update(this);
+    }
+
+    /** called by internal mechanisms, do not call yourself. */
+    @Generated(hash = 1638678461)
+    public void __setDaoSession(DaoSession daoSession) {
+        this.daoSession = daoSession;
+        myDao = daoSession != null ? daoSession.getUserDBDao() : null;
     }
 }
